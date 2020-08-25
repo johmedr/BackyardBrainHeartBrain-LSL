70544c33fba6b0a244285a3efe2f8c5164d3b882
Johan Medrano <johan.medrano653@gmail.com>
2020-08-25 15:02:05 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="byb_lsl_driver", # Replace with your own username
+    version="0.0.1",
+    author="Johan Medrano",
+    author_email="johan.medrano@umontpellier.fr",
+    description="A driver to connect the Heart&Brain kit from Backyard Brain to the LabStreamingLayer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       'pylsl',
+       'pyserial'
+    ],
+    python_requires='>=2.7',
+)
